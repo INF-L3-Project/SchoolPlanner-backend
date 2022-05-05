261ae4669923b649d60e76ef0e10a0a4339eed13
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-05-05 19:42:28 +0100

    Keep of the django secret key

--- schoolplanner/settings.py
+++ schoolplanner/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-15v(rzu7l2$oso3a=ahy8tl3(k=im6#pc%qc$f9_vpdqn5mq0m'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
